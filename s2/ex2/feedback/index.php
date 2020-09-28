02d68979651e9d5749c1c40d6e836982df299edc
Avaaren <avaaren@mail.ru>
2020-09-28 11:26:09 +0300

    Added mail template to feedback component call

--- s2/ex2/feedback/index.php
+++ s2/ex2/feedback/index.php
@@ -2,13 +2,18 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("feedback");
 ?><?$APPLICATION->IncludeComponent(
-	"bitrix:main.feedback",
-	"",
-	Array(
+	"bitrix:main.feedback", 
+	".default", 
+	array(
 		"EMAIL_TO" => "sale@bitrix.site",
-		"EVENT_MESSAGE_ID" => array(),
+		"EVENT_MESSAGE_ID" => array(
+			0 => "7",
+		),
 		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
-		"REQUIRED_FIELDS" => array(),
-		"USE_CAPTCHA" => "Y"
-	)
+		"REQUIRED_FIELDS" => array(
+		),
+		"USE_CAPTCHA" => "Y",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
