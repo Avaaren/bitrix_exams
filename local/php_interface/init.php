ceeb8160f668ba0fbebf519cc9d25a3f6f4fa643
Avaaren <avaaren@mail.ru>
2020-09-29 14:50:44 +0300

    Formated code. Added comments

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -6,12 +6,13 @@ function changeMetaData()
 {
     global $APPLICATION;
     CModule::IncludeModule("iblock");
+    // Получить урл загружаемой страницы
     $url = $APPLICATION->GetCurPage();
-    // print_r($url);
+    // Выбираем нужные свойства из созданного инфоблока (ID=10), если текущая страница есть в БД
     $arSelect = Array("ID", "IBLOCK_ID", "NAME", "PROPERTY_DESCRIPTION","PROPERTY_TITLE");
     $arFilter = Array("IBLOCK_ID"=>10, "NAME"=>$url);
     $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
-    
+    // Если поиск дал результат - извлекаем данные и устанавливаем свойства страницы
     if ( $ob = $res->Fetch() )
     {
         $APPLICATION->SetPageProperty("description", $ob["PROPERTY_DESCRIPTION_VALUE"]);
