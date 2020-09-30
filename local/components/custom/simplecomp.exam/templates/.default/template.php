a5fcd4e4ceced1ba74b19c16308a05aa1517ff8e
Avaaren <avaaren@mail.ru>
2020-09-30 11:43:39 +0300

    Added template for catalog simple component

--- local/components/custom/simplecomp.exam/templates/.default/template.php
+++ local/components/custom/simplecomp.exam/templates/.default/template.php
@@ -1,3 +1,33 @@
-<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-      echo $arResult['DATE'];
-?>
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+
+<div class="product-list-wraper">
+<?if(!empty($arResult["NEWS_CATALOG"])):?>
+	<div class="product-list-block">
+      <h3>Каталог</h3>
+            <?foreach($arResult["NEWS_CATALOG"] as $arItem):?>
+            <div class="product-header">
+            <b>
+                  <?echo $arItem["NEWS_NAME"]?> - <?echo $arItem["NEWS_DATE"]?>
+            </b> (
+                  <?foreach($arItem["CATEGORIES"] as $category):?>
+                        <?if ($key === array_key_last($array)):?>
+                              <?echo "$category"?>
+                        <?else:?> 
+                              <?echo "$category, "?> 
+                        <?endif;?>
+                  <?endforeach;?>
+                  )
+            </div> 
+
+            <ul class="product-list">
+                  <?foreach($arItem["PRODUCTS"] as $product):?>
+                  <li class="product-list-item">
+                        <?echo $product["NAME"]?> - <?echo $product["PROPERTY_PRICE_VALUE"]?> - <?echo $product["PROPERTY_MATERIAL_VALUE"]?> - <?echo $product["PROPERTY_ARTNUMBER_VALUE"]?>  
+                  </li>
+                  <?endforeach;?> 
+            </ul>
+            <?endforeach;?>
+      </div>
+<?endif;?>
+
+</div>
