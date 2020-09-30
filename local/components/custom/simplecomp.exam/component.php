33d3d816254b4a6aad87cbe828395331b1675694
Avaaren <avaaren@mail.ru>
2020-09-30 15:22:39 +0300

    Added advanced version of component logic

--- local/components/custom/simplecomp.exam/component.php
+++ local/components/custom/simplecomp.exam/component.php
@@ -1,4 +1,68 @@
 <? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 
+    $arFirm = array();
+    $arFilter = Array(
+        'IBLOCK_ID'=>$arParams["PRODUCTS_IBLOCK_ID"],
+        "ACTIVE"=>"Y",
+        "!PROPERTY_FIRMA" => false,
+        "CHECK_PERMISSIONS" => "Y"
+    );
+    $arSelect = Array("ID", "NAME", "PROPERTY_FIRMA");
+    $res = CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>50), $arSelect);
+    while($ar_result = $res->Fetch())
+    {
+
+            if ( array_key_exists($ar_result["PROPERTY_FIRMA_VALUE"], $arFirm) )
+            {
+                array_push($arFirm[$ar_result["PROPERTY_FIRMA_VALUE"]], $ar_result["ID"]);
+            }
+            else
+            {
+                $arFirm[$ar_result["PROPERTY_FIRMA_VALUE"]] = array($ar_result["ID"]);
+            }
+    }
+    $resultArray = array();
+    $counter = 0;
+    foreach( $arFirm as $key => $value )
+    {
+        $counter++;
+        $arSelect = Array("ID", "IBLOCK_ID", "NAME");
+        $arFilter = Array(
+            "IBLOCK_ID"=>$arParams["CLASS_IBLOCK_ID"], 
+            "ID"=>$key, 
+            "ACTIVE"=>"Y",
+            "CHECK_PERMISSIONS" => "Y"
+        );
+        $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
+
+        if ( $obFirm = $res->Fetch() )
+        {
+            $resultArray[$key] = array(
+                "FIRM_NAME" => $obFirm["NAME"],
+                "PRODUCTS" => array(),
+            );
+
+            $arSelect = Array(
+                "ID", "IBLOCK_ID", 'NAME', 
+                "PROPERTY_PRICE", "PROPERTY_MATERIAL", 
+                "PROPERTY_ARTNUMBER"
+            );
+            $arFilter = Array(
+                "IBLOCK_ID"=>$arParams["PRODUCTS_IBLOCK_ID"], 
+                "ID"=>$value, 
+                "ACTIVE"=>"Y",
+                "CHECK_PERMISSIONS" => "Y"
+            );
+            $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
+            
+            while( $productObject = $res->Fetch() )
+            {
+                array_push($resultArray[$key]["PRODUCTS"], $productObject);
+            }
+        }
+    }
+    $APPLICATION->SetTitle("Разделов - ".$counter); 
+    $arResult["CATALOG"] = $resultArray;
+
 $this->IncludeComponentTemplate();
 ?>
