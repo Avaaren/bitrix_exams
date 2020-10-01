1b9e35565867702ca78953466b98f2da77f09342
Avaaren <avaaren@mail.ru>
2020-10-01 11:28:58 +0300

    Added template for component

--- local/components/custom/simplecomp.exam/templates/.default/template.php
+++ local/components/custom/simplecomp.exam/templates/.default/template.php
@@ -1,2 +1,24 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
+<div class="news-list-wrapper">
+<?if ( $arResult["NON_AUTH"] == "Y" ):?>
+    <div class="error-message">
+        <p>Авторизируйтесь, чтобы просматривать контент</p>
+    </div>
+<?else:?>
+    <?foreach( $arResult["NEWS"] as $key => $value):?>
+        <div class="news-list-header">
+            <h4>
+                [<?echo $key;?>] - <?echo $value["AUTHOR"]["LOGIN"];?>
+            </h4>
+            <ul class="news-list">
+                <?foreach( $value["NEWS"] as $news_key => $news_value):?>
+                    <li class="news-list-item">
+                        <? echo $news_value["NAME"]; ?> - <?echo $news_value["DATE"]; ?>
+                    </li>
+                <?endforeach;?>
+            </ul>
+        </div>
+    <?endforeach;?>
+<?endif;?>
+</div>
