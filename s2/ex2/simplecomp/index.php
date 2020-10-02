0dc9035bd6380b1484b7f8cc33555a3cfd506390
Avaaren <avaaren@mail.ru>
2020-10-02 17:51:23 +0300

    Copied earlier component

--- s2/ex2/simplecomp/index.php
+++ s2/ex2/simplecomp/index.php
@@ -1,4 +1,18 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Простой компонент");
-?>Text here....<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?$APPLICATION->IncludeComponent(
+	"custom:simplecomp.exam", 
+	".default", 
+	array(
+		"CACHE_TIME" => "10",
+		"CACHE_TYPE" => "A",
+		"CLASS_IBLOCK_ID" => "11",
+		"LINK_TEMPLATE" => "",
+		"PRODUCTS_IBLOCK_ID" => "6",
+		"PRODUCT_PROPERTY_CODE" => "FIRMA",
+		"COMPONENT_TEMPLATE" => ".default",
+		"NUM_ELEMENTS" => "2"
+	),
+	false
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
