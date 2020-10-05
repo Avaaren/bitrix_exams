87fb6aa32499ac849a2c120350c1adf22afc4e30
Avaaren <avaaren@mail.ru>
2020-10-05 12:31:58 +0300

    Added perfomance control result

--- s2/ex2/time_control.php
+++ s2/ex2/time_control.php
@@ -1,8 +1,9 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Оценка производительности");
-?>
-
-Text here....
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>Наиболее тяжелая страница:<br>
+ /s2/news/index.php - 40.39% - 2.92 сек<br>
+ <br>
+ Компоненты с наибольшим числом запросов:<br>
+bitrix:furniture.catalog.random - 5 запросов - 0.0053 сек<br>
+bitrix:news.list - 4 запроса - 0.01сек<br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
