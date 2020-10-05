b2e9dd7badfb3bd9a47fa8a25814cdd61bd92308
Avaaren <avaaren@mail.ru>
2020-10-05 12:12:27 +0300

    Added perfomance info to special page

--- s2/ex2/time-check.php
+++ s2/ex2/time-check.php
@@ -1,8 +1,8 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Оценка производительности");
-?>
-
-Text here....
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>Самые тяжелые страницы: <br>
+ /s2/news/index.php - 17.32% - 1.3 c. <br>
+ <br>
+ Самый&nbsp;тяжелый компонент: &nbsp;<br>
+bitrix:furniture.catalog.random - 0.0484 с. - отображает случайный элемент каталога во включаемой области страницы<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
