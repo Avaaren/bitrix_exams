e36bcc91f021e61246d38e384db88a205ac98379
Avaaren <avaaren@mail.ru>
2020-10-06 15:15:58 +0300

    Added exampage content

--- local/components/custom/complexcomp.exam/templates/.default/exampage.php
+++ local/components/custom/complexcomp.exam/templates/.default/exampage.php
@@ -2,5 +2,14 @@
 ?>
 
 <?
-
+if ($arParams['SEF_MODE'] == "Y")
+{
+    echo "PARAM_1 = ".$arResult['VARIABLES']["PARAM_1"];
+    echo "<br> PARAM_2 = ".$arResult['VARIABLES']["PARAM_2"];
+}
+else
+{
+    echo "PARAM_1 = ".$_REQUEST[$arParams['VARIABLE_ALIASES']['PARAM_1']];
+    echo "<br> PARAM_2 = ".$_REQUEST[$arParams['VARIABLE_ALIASES']['PARAM_2']];
+}
 ?>
